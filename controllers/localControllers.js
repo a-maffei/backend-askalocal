1b8638e43a98c80007fffe6293bfec9516118400
Alessandra Maffei <ales.maffei@gmail.com>
2023-02-01 11:20:34 +0100

    fixed put local controller

--- controllers/localControllers.js
+++ controllers/localControllers.js
@@ -148,7 +148,7 @@ const updateLocal = async (req, res) => {
 
   try {
     const updatedLocal = await Local.updateOne(
-      { id: req.params.id },
+      { email: email },
       {
         $set: {
           firstname,
@@ -160,12 +160,14 @@ const updateLocal = async (req, res) => {
           pic,
           bio,
           language,
-          emailP,
-          callP,
-          flatP,
-          appointmentP,
-          serviceP,
-          interviewP,
+          categories: {
+            emailP,
+            callP,
+            flatP,
+            appointmentP,
+            serviceP,
+            interviewP,
+          },
         },
       }
     );
